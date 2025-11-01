6814cfde5b195fcf163036717ffa936bf3bd0d72
pro-masud <promasudbd@gmail.com>
2025-11-01 18:19:10 +0600

    feat: task list dynamic successfully

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -45,28 +45,17 @@ const TaskList = ({ tasks }) => {
                 </td>
                 <td>{task.title}</td>
                 <td>
-                  <div>
-                    Connect an existing API to a third-party database using
-                    secure methods and handle data exchange efficiently.
-                  </div>
+                  <div>{task.description}</div>
                 </td>
                 <td>
                   <ul className="flex justify-center gap-1.5 flex-wrap">
-                    <li>
-                      <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
-                        Web
-                      </span>
-                    </li>
-                    <li>
-                      <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#1C92FFB0] px-2.5 text-sm capitalize text-[#F4F5F6]">
-                        Python
-                      </span>
-                    </li>
-                    <li>
-                      <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#FE1A1AB5] px-2.5 text-sm capitalize text-[#F4F5F6]">
-                        API
-                      </span>
-                    </li>
+                    {task.tags.map((tag) => (
+                      <li>
+                        <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
+                          {tag}
+                        </span>
+                      </li>
+                    ))}
                   </ul>
                 </td>
                 <td className="text-center">High</td>
